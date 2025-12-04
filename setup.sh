7fac9513800486003230627a28889a36f161f0bf
alexandruursulescu <ursulescu.alexandru.ext@siemens-energy.com>
2025-12-04 15:51:05 +0200

    push

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-find . -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+find . \
+  -type f \( -name "*.yml" -o -name "*.yaml" -o -name "*.json" -o -name "*.sh" -o -name "*.txt" \) \
+  -exec sed -i '' "s/ursu96/$1/g" {} +
